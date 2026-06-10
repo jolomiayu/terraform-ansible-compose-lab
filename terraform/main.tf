6365b376f7bd162b2455b99d29ce72309490eb51
Ubuntu <ubuntu@ip-172-31-15-204.eu-west-1.compute.internal>
2026-06-10 07:35:05 +0000

    Add Terraform Ansible Docker Compose Lab

---
+++ terraform/main.tf
@@ -0,0 +1,46 @@
+resource "aws_security_group" "web_sg" {
+  name = "compose-web-sg"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 3000
+    to_port     = 3000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_instance" "web" {
+  ami           = "ami-0df368112825f8d8f"
+  instance_type = "t3.micro"
+
+  key_name = "ansible-key"
+
+  security_groups = [
+    aws_security_group.web_sg.name
+  ]
+
+  tags = {
+    Name = "compose-web-server"
+  }
+}
